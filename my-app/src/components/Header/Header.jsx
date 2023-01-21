b3d8dfef565724b8c53d2c6710b996a7c575ede9
Ratushnyy Andrey <90136453+Oper992@users.noreply.github.com>
2023-01-21 19:36:08 +0200

    creating...

--- my-app/src/components/Header/Header.jsx
+++ my-app/src/components/Header/Header.jsx
@@ -1,40 +1,37 @@
 import { HandySvg } from "handy-svg";
 import OpenBookIcon from "../../images/images/header/fa-solid_book-open.svg";
 import style from "./Header.module.scss";
-import { Mobile, Tablet, Desktop } from "../../helpers/responsiveComponents";
 
 export const Header = () => {
   return (
-    <Desktop>
-      <div className="container">
-        <div className={style.header}>
-          <div className={style.headerSvg}>
-            <HandySvg src={OpenBookIcon} width="40" height="35" />
-          </div>
-          <p className={style.logo}>
-            <span className={style.logoPart}>Finance</span> Ledger
-          </p>
-          <nav>
-            <ul className={style.navList}>
-              <li className={style.navItem}>
-                <button className={style.navButton}>Home</button>
-              </li>
-              <li className={style.navItem}>
-                <button className={style.navButton}>About</button>
-              </li>
-              <li className={style.navItem}>
-                <button className={style.navButton}>Cases</button>
-              </li>
-              <li className={style.navItem}>
-                <button className={style.navButton}>Blog</button>
-              </li>
-              <li className={style.navItem}>
-                <button className={style.navButton}>Contact</button>
-              </li>
-            </ul>
-          </nav>
+    <div className="container">
+      <div className={style.header}>
+        <div className={style.headerSvg}>
+          <HandySvg src={OpenBookIcon} width="40" height="35" />
         </div>
+        <p className={style.logo}>
+          <span className={style.logoPart}>Finance</span> Ledger
+        </p>
+        <nav>
+          <ul className={style.navList}>
+            <li className={style.navItem}>
+              <button className={style.navButton}>Home</button>
+            </li>
+            <li className={style.navItem}>
+              <button className={style.navButton}>About</button>
+            </li>
+            <li className={style.navItem}>
+              <button className={style.navButton}>Cases</button>
+            </li>
+            <li className={style.navItem}>
+              <button className={style.navButton}>Blog</button>
+            </li>
+            <li className={style.navItem}>
+              <button className={style.navButton}>Contact</button>
+            </li>
+          </ul>
+        </nav>
       </div>
-    </Desktop>
+    </div>
   );
 };
