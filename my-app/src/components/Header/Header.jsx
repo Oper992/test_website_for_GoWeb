ad81d22a3a73cfd620d73b8b897e7c5552ee413d
Ratushnyy Andrey <90136453+Oper992@users.noreply.github.com>
2023-01-21 21:26:01 +0200

    creating hero

--- my-app/src/components/Header/Header.jsx
+++ my-app/src/components/Header/Header.jsx
@@ -4,33 +4,35 @@ import style from "./Header.module.scss";
 
 export const Header = () => {
   return (
-    <div className="container">
-      <div className={style.header}>
-        <div className={style.headerSvg}>
-          <HandySvg src={OpenBookIcon} width="40" height="35" />
+    <div className={style.header}>
+      <div className="container">
+        <div className={style.content}>
+          <div className={style.logo}>
+            <HandySvg className={style.logoIcon} src={OpenBookIcon} />
+            <p>
+              <span className={style.logoPart}>Finance</span> Ledger
+            </p>
+          </div>
+          <nav className={style.nav}>
+            <ul className={style.navList}>
+              <li className={style.navItem}>
+                <button className={style.navButton}>Home</button>
+              </li>
+              <li className={style.navItem}>
+                <button className={style.navButton}>About</button>
+              </li>
+              <li className={style.navItem}>
+                <button className={style.navButton}>Cases</button>
+              </li>
+              <li className={style.navItem}>
+                <button className={style.navButton}>Blog</button>
+              </li>
+              <li className={style.navItem}>
+                <button className={style.navButton}>Contact</button>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <p className={style.logo}>
-          <span className={style.logoPart}>Finance</span> Ledger
-        </p>
-        <nav>
-          <ul className={style.navList}>
-            <li className={style.navItem}>
-              <button className={style.navButton}>Home</button>
-            </li>
-            <li className={style.navItem}>
-              <button className={style.navButton}>About</button>
-            </li>
-            <li className={style.navItem}>
-              <button className={style.navButton}>Cases</button>
-            </li>
-            <li className={style.navItem}>
-              <button className={style.navButton}>Blog</button>
-            </li>
-            <li className={style.navItem}>
-              <button className={style.navButton}>Contact</button>
-            </li>
-          </ul>
-        </nav>
       </div>
     </div>
   );
--- my-app/src/components/Hero/Hero.jsx
+++ my-app/src/components/Hero/Hero.jsx
@@ -3,9 +3,11 @@ import style from "./Hero.module.css";
 export const Hero = () => {
   return (
     <section>
-      <div className="container">
-        <h1>The Sky Is The Limit</h1>
-        <p>We provide world class financial assistance</p>
+      <div className={style.hero}>
+        <div className="container">
+          <h1 className={style.heroTitle}>The Sky Is The Limit</h1>
+          <p>We provide world class financial assistance</p>
+        </div>
       </div>
     </section>
   );
