290ec3d31fa643c4c7c679b45b0b2276adc87f1b
Ratushnyy Andrey <90136453+Oper992@users.noreply.github.com>
2023-01-22 14:03:13 +0200

    ceating

--- my-app/src/components/Hero/Hero.jsx
+++ my-app/src/components/Hero/Hero.jsx
@@ -1,4 +1,6 @@
 import style from "./Hero.module.scss";
+import iconArrow from "../../images/hero/fa-solid_angle-right.svg";
+import { HandySvg } from "handy-svg";
 
 export const Hero = () => {
   return (
@@ -6,7 +8,13 @@ export const Hero = () => {
       <div className={style.hero}>
         <div className="container">
           <h1 className={style.heroTitle}>The Sky Is The Limit</h1>
-          <p>We provide world class financial assistance</p>
+          <p className={style.heroPostTitle}>
+            We provide world class financial assistance
+          </p>
+          <button type="button" className={style.heroButton}>
+            <HandySvg className={style.iconHero} src={iconArrow} width="9" height="18" />
+            Read More
+          </button>
         </div>
       </div>
     </section>
